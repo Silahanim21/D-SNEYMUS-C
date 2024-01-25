9bb1e3869ef0d738785537a0cd4bb309e5daca31
Deneme3421 <152818838+Deneme3421@users.noreply.github.com>
2024-01-26 02:40:43 +0300

    Update active.py

--- LOKESHXMUSIC/plugins/tools/active.py
+++ LOKESHXMUSIC/plugins/tools/active.py
@@ -12,9 +12,9 @@ from LOKESHXMUSIC.utils.database import (
 )
 
 
-@app.on_message(filters.command(["activevc", "activevoice"]) & SUDOERS)
+@app.on_message(filters.command(["aktifses", "aktifsesli"]) & SUDOERS)
 async def activevc(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("👉 AKTİF SESLİ SOHBETLER ALINMA LISTESI...")
     served_chats = await get_active_chats()
     text = ""
     j = 0
@@ -36,17 +36,17 @@ async def activevc(_, message: Message):
         except:
             continue
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(f" 👉 AKTİF SESLİ SOHBET YOK  {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛs :</b>\n\n{text}",
+           f"<b> 👉ŞU ANDA AKTİF SESLİ SOHBETLERİN LİSTESİ  :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
 
 
-@app.on_message(filters.command(["activev", "activevideo"]) & SUDOERS)
+@app.on_message(filters.command(["aktifvideo", "akdivvide"]) & SUDOERS)
 async def activevi_(_, message: Message):
-    mystic = await message.reply_text("» ɢᴇᴛᴛɪɴɢ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ʟɪsᴛ...")
+    mystic = await message.reply_text("👉 AKTİF GÖRÜNTÜLÜ SOHBET LİSTESİ ALINMA..")
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
@@ -68,9 +68,9 @@ async def activevi_(_, message: Message):
         except:
             continue
     if not text:
-        await mystic.edit_text(f"» ɴᴏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs ᴏɴ {app.mention}.")
+        await mystic.edit_text(f"👉 AKTİF GÖRÜNTÜLÜ SOHBET YOK  {app.mention}.")
     else:
         await mystic.edit_text(
-            f"<b>» ʟɪsᴛ ᴏғ ᴄᴜʀʀᴇɴᴛʟʏ ᴀᴄᴛɪᴠᴇ ᴠɪᴅᴇᴏ ᴄʜᴀᴛs :</b>\n\n{text}",
+          f"<b>»ŞU ANDA AKTİF GÖRÜNTÜLÜ SOHBETLERİN LİSTESİ :</b>\n\n{text}",
             disable_web_page_preview=True,
         )
