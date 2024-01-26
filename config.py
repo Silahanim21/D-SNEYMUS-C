1178609a234a5833942f9b11feda769e036fca18
Ali <104919540+olurunabirak@users.noreply.github.com>
2024-01-26 13:35:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", 
-    "https://github.com/Deneme3421/Disnep"
+    "https://github.com/olurunabirak/Anonim-turkce"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
