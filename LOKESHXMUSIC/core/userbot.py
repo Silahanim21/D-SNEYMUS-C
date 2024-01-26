17abf5950ca42500a6330032ad1c027b2e489bb2
Deneme3421 <152818838+Deneme3421@users.noreply.github.com>
2024-01-26 03:14:01 +0300

    Update userbot.py

--- LOKESHXMUSIC/core/userbot.py
+++ LOKESHXMUSIC/core/userbot.py
@@ -102,7 +102,7 @@ class Userbot(Client):
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "🔷asistan basladı)
+                await self.three.send_message(config.LOGGER_ID, "🔷asistan basladı...")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 3 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -124,7 +124,7 @@ class Userbot(Client):
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "🔷Assistan basladı)
+                await self.four.send_message(config.LOGGER_ID, "🔷Assistan basladı..")
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 4 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
@@ -146,7 +146,7 @@ class Userbot(Client):
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "🔷Assitan basladı" )
+                await self.five.send_message(config.LOGGER_ID, "🔷Assitan basladı.." )
             except:
                 LOGGER(__name__).error(
                     "Assistant Account 5 has failed to access the log Group. Make sure that you have added your assistant to your log group and promoted as admin! "
