033283188963a58c1ce5931e38eebda3952e0ab8
ayaz2112 <157922890+ayaz2112@users.noreply.github.com>
2024-01-28 00:13:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", 
-    "https://github.com/olurunabirak/Anonim-turkce"
+    "https://github.com/ayaz2112/Dene"
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
