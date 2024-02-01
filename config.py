da4b8f55059a2388012819162f92d397d6ae792a
Ali <104919540+olurunabirak@users.noreply.github.com>
2024-02-01 04:12:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://ce3421:ce3421@cluster0.5dx4
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1800))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = -1002112138580
+LOGGER_ID = -1002143049926
 LOG = 2
 # Get this value from @BRANDRD_ROBOT on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID","5693410792" ))
