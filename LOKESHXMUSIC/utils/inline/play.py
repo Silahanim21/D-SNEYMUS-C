6dfb5dafe37e653b9f0b54b3423eabd0e0c4e3b8
Ali <104919540+olurunabirak@users.noreply.github.com>
2024-03-14 18:21:20 +0300

    Update play.py

--- LOKESHXMUSIC/utils/inline/play.py
+++ LOKESHXMUSIC/utils/inline/play.py
@@ -73,8 +73,6 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-        ],
-        [
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
@@ -89,8 +87,6 @@ def stream_markup(_, chat_id):
     [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-        ],
-        [
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         
