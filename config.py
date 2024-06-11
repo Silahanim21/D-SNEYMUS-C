f4e2f0593694a0176611cf687bc24662bffb642f
Son3421 <son342121@gmail.com>
2024-06-11 17:21:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,6 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/disneykomutt")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/disneykomutt")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -103,12 +102,6 @@ def time_to_seconds(time):
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
-if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
-        raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
-        )
-        
 if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
         raise SystemExit(
